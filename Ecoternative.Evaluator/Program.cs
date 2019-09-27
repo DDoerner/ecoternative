abed66d9e2dc786de07fa04763f60e7147683bcd
DDoerner <18138239+DDoerner@users.noreply.github.com>
2019-09-28 00:16:40 +0200

    Initial commit

---
+++ Ecoternative.Evaluator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ecoternative.Evaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
