9920956002433cb8811aee7159ae2468d41713b4
TobiasRoeddiger <usdkr@student.kit.edu>
2019-09-28 17:49:43 +0200

    fixed typo

--- Ecoternative.Chrome/backgroundScript.js
+++ Ecoternative.Chrome/backgroundScript.js
@@ -44,7 +44,7 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 });
 
 chrome.tabs.onActivated.addListener( function (tabId, changeInfo, tab) {
-    if (url === undefined) 
+    if (tab.url === undefined) 
     {
         updateClimateScore(undefined)
     }
